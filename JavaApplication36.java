fcabd8c0400298400d9af365658ba1078c6dac2d
hilalariboga34 <150022719+hilalariboga34@users.noreply.github.com>
2023-12-30 14:46:51 +0300

    Add files via upload

---
+++ JavaApplication36.java
@@ -0,0 +1,37 @@
+
+package javaapplication36;
+
+import java.util.Scanner;
+
+
+public class JavaApplication36 {
+
+    
+    public static void main(String[] args) {
+       
+      /*  Scanner klavye=new Scanner(System.in);
+        int mat , turkce , fizik , kimya , tarih , müzik;
+        System.out.print("Matematik notunu giriniz:");
+        mat=klavye.nextInt();
+        System.out.print("Turkce notunu giriniz:");
+        turkce=klavye.nextInt();
+        System.out.print("Fizik notunuzu giriniz:");
+        fizik=klavye.nextInt();
+        System.out.print("Kimya notunuzu giriniz:");
+        kimya=klavye.nextInt();
+        System.out.print("Tarih notunuzu giriniz:");
+        tarih=klavye.nextInt();
+        System.out.print("Müzik notunuzu giriniz:");
+        müzik=klavye.nextInt();
+        
+        int top=(mat+kimya+turkce+fizik+tarih+müzik);
+        double ort=top/6;
+        
+        boolean gecti=ort>60;
+       System.out.println(gecti ? "GECTİNİZ" : "KALDINIZ!");
+       
+                */
+      
+    }
+    
+}
